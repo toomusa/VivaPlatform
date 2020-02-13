ee0f668ee93829f78b9dfbe05baa3620b49d2623
toomusa <3895499+toomusa@users.noreply.github.com>
2020-02-12 23:58:16 -0800

    fix html route

--- index.js
+++ index.js
@@ -31,9 +31,6 @@ app.use(routes, (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    app.get("*", (req, res) =>
-        res.sendFile(path.join(__dirname, "./client/build/index.html"))
-    );
 }
 
 const PORT = process.env.PORT || 3001;
