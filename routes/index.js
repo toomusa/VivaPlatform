d8a989528ad947a5dbd7c539ded6383c0ef11663
toomusa <3895499+toomusa@users.noreply.github.com>
2020-02-13 00:05:31 -0800

    fix html route

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ const apiRoutes = require("./apiRoutes");
 
 router.use("/auth", authRoutes);
 router.use("/api", apiRoutes);
-router.use("/", htmlRoutes);
+// router.use("/", htmlRoutes);
+
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
